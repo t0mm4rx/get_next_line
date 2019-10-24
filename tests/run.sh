16193fd5da266cc56047528533f0ed8921344e6d
t0mm4rx <tom.marx@hotmail.fr>
2019-10-25 00:58:29 +0200

    tests

--- tests/run.sh
+++ tests/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gcc -Wall -Wextra -Werror -g3 -fsanitize=address -D BUFFER_SIZE=$1 ../get_next_line.c ../get_next_line_utils.c main.c  && ./a.out $2
+gcc -g -fsanitize=address -Wall -Wextra -Werror -D BUFFER_SIZE=$1 ../get_next_line.c ../get_next_line_utils.c main.c  && ./a.out $2
