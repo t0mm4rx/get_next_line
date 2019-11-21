801ae73362cad4392b1b3455a217fc335d63751b
Tom <tom.marx@hotmail.fr>
2019-11-21 17:22:38 +0100

    tests

--- tests/run.sh
+++ tests/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gcc -g -fsanitize=address -Wall -Wextra -Werror -D BUFFER_SIZE=$1 ../get_next_line.c ../get_next_line_utils.c main.c  && ./a.out $2
+gcc -g -fsanitize=address -Wall -Wextra -Werror -D BUFFER_SIZE=$1 ../get_next_line_bonus.c ../get_next_line_utils_bonus.c main.c  && ./a.out $2
